b398d02f1d3d97ac37754818044935453e8eb89e
Sierraaaaaaaaaaa <166534818+Sierraaaaaaaaaaa@users.noreply.github.com>
2024-11-29 17:46:05 +0700

    fix some thing and add gitignore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Report from "./components/Report";
 import Manage from "./components/Manage";
 import PrintHistory from "./components/PrintHis";
+import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return <Manage/>;
 }
---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,50 @@
+import { Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+import api from "../api";
+import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants";
+import { useEffect, useState } from "react";
+
+
+function ProtectedRoute({children}){
+    const [isAuthorized, setIsAuthorized] = useState(null);
+
+    useEffect(() => {
+        auth().catch(() => setIsAuthorized(false));
+    }, [])
+
+    const refreshToken = async () => {
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+        try {
+            const response = await api.post("api/token/refresh/", {
+                refresh: refreshToken,
+            });
+            if (response.status === 200) {
+                localStorage.setItem(ACCESS_TOKEN, response.data.access);
+                setIsAuthorized(true);
+            }
+        }
+        catch (error) {
+            console.error(error);
+            setIsAuthorized(false);
+        }
+    };
+    const auth = async () => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (!token) {
+            setIsAuthorized(false);
+            return;
+        }
+        const decoded = jwtDecode(token);
+        if (decoded.exp * 1000 < Date.now()) {
+            await refreshToken();
+        }
+        setIsAuthorized(true);  
+    }
+    if (isAuthorized === null) {
+        return <div>Loading...</div>;
+    }
+    return isAuthorized ? children : <Navigate to="/login" />;
+}
+
+export default ProtectedRoute;
+
