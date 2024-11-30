c6cfb6bfe58bb7d99fb1e931480b522b3cd3c47d
Sierraaaaaaaaaaa <166534818+Sierraaaaaaaaaaa@users.noreply.github.com>
2024-12-01 03:07:45 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,29 +16,36 @@ import ViewPrinterPage from './app/homepage/viewprinter/PrinterPage';
 import ViewPrinterHistoryPage from './app/homepage/viewprinterhistory/ViewPrinterHis';
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div id="root">
+        <Navbar />
+        <div className="main-content">
+          <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Manage />
+              </ProtectedRoute>
+            }
+          />
+            <Route path="/createaccount" element={<CreateAccPage />} />
+            <Route path="/buyingpermis" element={<BuyPermisPage />} />
+            <Route path="/manageactivity" element={<ManageActPage/>} />
+            <Route path="/viewpayment" element={<ViewPaymentPage/>}/>
+            <Route path="/viewprint_manager" element={<ViewPrintingHistory/>}/>
+            <Route path="/viewprinter" element={<ViewPrinterPage/>}/>
+            <Route path="/viewprinterhistory" element={<ViewPrinterHistoryPage />} />
+            <Route path="/manage" element={<Manage />} />
+            <Route path="/printhistory" element={<PrintHistory />} />
+            <Route path="/report" element={<Report />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </BrowserRouter>
+  );
 }
 
 export default App;
